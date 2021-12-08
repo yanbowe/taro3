09fb6371830f7a954f5f8cd5e579fb7ce6220206
Yanbowen <349952469@qq.com>
2021-12-08 16:51:12 +0800

    nutui 折叠面板问题

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import './app.scss'
-import { Icon } from '@nutui/nutui-taro'
+import { Icon, Collapse, CollapseItem } from '@nutui/nutui-taro'
 import '@nutui/nutui-taro/dist/style.css'
 import { setupStore } from '@/stores'
 
 const App = createApp({
   onShow() {}
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-}).use(Icon)
+})
+  .use(Icon)
+  .use(Collapse)
+  .use(CollapseItem)
 setupStore(App)
 export default App
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,26 @@
 <template>
   <view class="page-body">
-    <view class="page-section">
-      <text>地区选择器{{ index }} </text>
-      <view>
-        <picker mode="selector" :range="selector" @change="onChange" :value="index">
-          <view class="picker"> 当前选择：{{ selectorChecked }} </view>
-        </picker>
-      </view>
-    </view>
+    <nut-collapse v-model:active="activeName" :accordion="true" icon="down-arrow">
+      <nut-collapse-item :title="title1" :name="1">
+        <view> 基于京东设计语言体系，构建场景</view>
+      </nut-collapse-item>
+      <nut-collapse-item :title="title2" :name="2" :sub-title="subTitle">
+        提高界⾯的模块化通用程度
+      </nut-collapse-item>
+      <nut-collapse-item :title="title3" :name="3">
+        采用组合式 API Composition 语法重构，结构清晰，功能模块化
+      </nut-collapse-item>
+    </nut-collapse>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-const index = ref(0);
-const selectorChecked = ref("美国");
-
-const selector = ["美国", "中国", "巴西", "日本"];
-function onChange(e) {
-  selectorChecked.value = selector[e.detail.value];
-  index.value = selector.findIndex((v) => v === selectorChecked.value);
-}
+import { ref, reactive } from "vue";
+const activeName = ref(1);
+const subTitle = "副标题";
+const title = reactive({
+  title1: "标题1",
+  title2: "标题2",
+  title3: "标题3",
+});
 </script>
